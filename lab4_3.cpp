0e46a74209cb96294ce28222bd129da34ff7168f
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-22 19:26:42 +0700

    coding...

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -3,7 +3,7 @@ int main()
 {
 	int cont;
 	scanf("%d", &cont);
-	int score[cont];
+	int score[cont, receive];
 	for (int i = 0; i < cont; i++)
 	{
 		for (j = 0; j < cont; j++)
@@ -12,8 +12,13 @@ int main()
 			{
 				continue;
 			}
-			
+			scanf("%d ", &receive);
+			if (receive >= 1 && receive <= 5)
+			{
+				score[j] = receive;
+			}
 		}
 	}
+	
 	return 0;
 }
