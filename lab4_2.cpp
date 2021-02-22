6cf6658053353853cbf58413d430c2b4f2b11b04
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-22 19:06:56 +0700

    add more case

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main()
 {
-	int prime[50000] = {}, target = 100000, max = 0, j, find = 2;
+	int prime[50000] = {}, target, max = 0, j, find = 2;
 	prime[0] = 2;
-	while (find < target && find <= 100000)
+	scanf("%d", &target);
+	while (find < target && find <= 100050)
 	{
 		next:
 			find++;
@@ -16,9 +17,28 @@ int main()
 			}
 			j--;
 		}
-		printf("%d\n", find); //test
+		printf("%d\n", find);
 		prime[max+1] = find;
 		max++;
 	}
+	if (find != prime[max])
+	{
+		loop:
+			find++;
+		j = max;
+		while (j >= 0)
+		{
+			if (find%prime[j] == 0)
+			{
+				goto loop;
+			}
+			j--;
+		}
+		printf("%d\n", find);
+		prime[max+1] = find;
+		max++;
+		goto breaking;
+	}
+	breaking:
 	return 0;
 }
