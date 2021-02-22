d807d88f4a318f7005591252292c566fae4bf810
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-22 18:58:08 +0700

    add next (break)

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 int main()
 {
-	int prime[50000] = {}, target, max = 0, j, find = 3;
+	int prime[50000] = {}, target, max = 0, j, find = 2;
 	prime[0] = 2;
 	while (find < target && find <= 100000)
 	{
+		next:
+			find++;
 		j = i;
-		while ()
+		while (j >= 0)
+		{
+			
+			j--;
+		}
 	}
 	return 0;
 }
