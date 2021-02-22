dd0b676b57313c54c8cee8b353e239114efd2c2a
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2021-02-22 18:55:14 +0700

    add more var

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	int prime[50000] = {}, target, i = 0;
+	int prime[50000] = {}, target, i = 0, j, find;
 	prime[0] = 2;
 	while (i < target && i < 50000)
 	{
-		
+		j = i;
+		while ()
 	}
 	return 0;
 }
